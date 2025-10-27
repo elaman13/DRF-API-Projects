3ded369c3ad4c4f71039223451fad0957b59c253
rojaz <beimnet.mi1pa@gmail.com>
2025-10-27 18:57:41 +0300

    add signup view and serializer.

--- social-media-api/api/views.py
+++ social-media-api/api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import SignUpSerializer
 
 # Create your views here.
+class SignUpView(generics.CreateAPIView):
+    serializer_class = SignUpSerializer
