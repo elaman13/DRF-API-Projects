ea3ac6cd35cfba59b45d5a31870e20e53d3f3e38
rojaz <beimnet.mi1pa@gmail.com>
2025-10-27 19:00:10 +0300

    add token obtain and refresh view

--- social-media-api/api/urls.py
+++ social-media-api/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import SignUpView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh')
 ]
 
