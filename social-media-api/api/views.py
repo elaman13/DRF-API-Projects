6874fc06c3aff95a971d972df1204e51e1453745
rojaz <beimnet.mi1pa@gmail.com>
2025-10-27 19:08:45 +0300

    add jwt for all view and add token and refresh lifetime

--- social-media-api/api/views.py
+++ social-media-api/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .serializers import SignUpSerializer
 
-# Create your views here.
+
 class SignUpView(generics.CreateAPIView):
     serializer_class = SignUpSerializer
