c03e4061e9fa41ca2b394d91aff479af1853510c
Elyas <elaman99620@gmail.com>
2025-10-27 12:29:35 +0300

    create follow model.

--- social-media-api/app/models.py
+++ social-media-api/app/models.py
@@ -31,3 +31,10 @@ class Like(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
+
+    class Meta:
+        unique_together = ('follower', 'following')
