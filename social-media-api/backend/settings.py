07a8a8c49709ec04a3025179eb82249728241e9e
elyas <elaman99620@gmail.com>
2025-10-29 09:42:59 +0300

    add pagination

--- social-media-api/backend/settings.py
+++ social-media-api/backend/settings.py
@@ -120,7 +120,11 @@ AUTH_USER_MODEL = 'app.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+
+    # add pagination to the post
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
 }
 
 
