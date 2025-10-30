bc6717c01e822e4799d7b0e027d051c66c727d14
elyas <elaman99620@gmail.com>
2025-10-30 21:13:48 +0300

    remove time tracker api

--- time-tracker-api/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TimeCreateView, TimeListView, TimeView
-
-urlpatterns = [
-    path('time/', TimeCreateView.as_view(), name='time'),
-    path('times/', TimeListView.as_view(), name='times'),
-    path('times/<int:pk>/', TimeView.as_view(), name='time-detail'),
-]
--- time-tracker-api/app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Time(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField(null=True, blank=True)
